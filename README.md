8f8f0296ea85183bec44edf1594e7f1fdf728871
Avi Schwab <schwab@palantir.net>
2016-12-27 20:55:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# durandal-init
---
+++ install.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+# This is heavily inspired by the amazing Michelle
+# https://github.com/craychee/loki-init
+# Saints are silly.
+
+function pause() {
+  read -p "$*"
+}
+
+CWD=($PWD)
+
+cd ~
+
+# We can't get them directly, but just click this button :)
+xcode-select --install
+sudo xcodebuild -license accept
+
+pause 'Press [Enter] once you have installed XCode and XCode Command Line Tools.'
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Installing software...
+brew install ruby
+brew install git
+brew install wget
+
+brew tap caskroom/cask
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/binary
+brew tap caskroom/versions
+
+brew install brew-cask
+
+# Browsers
+brew cask install google-chrome
+# Don't kid yourself... you'll eventually need it
+brew cask install firefox
+
+# Random Mac tools
+brew cask install alfred # Do all the things
+brew cask install lastpass # Personal passwords
+brew cask install 1password # Work passwords
+brew cask install hipchat # You know... 'cause we're hip
+brew cask install macdown # Write better README.md's
+brew cask install spotify # 🎶
+
+# Install the MacAppStore CLI and git some apps.
+# If you haven't purchased these in the App Store, this won't work.
+brew install mas
+mas signin froboy@gmail.com
+mas install 1179623856 # Pastebot
+mas install 899972312 # WordService
+mas install 406056744 # Evernote
+mas install 417375580 # BetterSnapTool
+mas install 405772121 # LittleIpsum
+mas install 668208984 # GIPHY CAPTURE
+mas install 803453959 # Slack
+mas install 975937182 # Fantastical 2
+mas install 425955336 # Skitch
+mas install 557168941 # Tweetbot
+# id's above are tied to specific versions.
+# They're most certainly out of date...
+mas upgrade
+
+# Dev tools
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install sequel-pro
+brew cask install sourcetree
+brew cask install torguardf
+brew install node
+brew install npm
+
+# Be sure we can vagrant
+brew install ansible
+vagrant plugin install vagrant-hostmanager
+vagrant plugin install vagrant-auto_network
+
+echo "Setting up Atom... you'll have to enter your deets in Settings:Sync Settings"
+brew cask install atom
+apm install sync-settings
+
+# Set up PHP tools
+# https://philsturgeon.uk/php/2013/08/20/php-static-analysis-in-sublime-text/
+# http://benmatselby.github.io/sublime-phpcs/
+echo "Setting up PHP Tools"
+sudo chown -R `whoami` /usr/local
+brew tap homebrew/homebrew-php
+brew install phpmd
+brew install php-code-sniffer
+brew install php-cs-fixer
+
+# Do the Mac thing that you have to do but you shouldn't have to do
+# This https://github.com/jirsbek/SSH-keys-in-macOS-Sierra-keychain
+curl -o ~/Library/LaunchAgents/ssh.add.a.plist https://raw.githubusercontent.com/jirsbek/SSH-keys-in-macOS-Sierra-keychain/master/ssh.add.a.plist
+
+# Hold my own hand to make sure I finish configuring.
+echo "Add your ssh keys (you put them in your secret hiding place)."
+pause 'Press [Enter] when you have added your ssh key.'
+chmod 400 ~/.ssh/*
+
+# Move preference files
+echo "Setting up your preference files."
+cp .gitconfig ~/.gitconfig
+cp .gitignore_global ~/.gitignore_global
+cp .profile ~/.profile
+
+# Get git things
+curl -o ~/usr/local/etc/bash_completion.d/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -o ~/usr/local/etc/bash_completion.d/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+echo "NICE WORK! Now get going... THEY'RE EVERYWHERE!!!"
